9740b5c3ba260d652d5b0263b0ef50958c550c3c
busters0325 <59666419+busters0325@users.noreply.github.com>
2020-03-31 10:43:28 +0800

    Add files via upload

--- file.hs
+++ file.hs
@@ -6,21 +6,28 @@ import Data.Typeable
 
 main = do  
         let list = []
-        handle <- openFile "test.txt" ReadMode
+        handle <- openFile "read.txt" ReadMode
         contents <- hGetContents handle
         let singlewords = words contents
             list = f singlewords
+
+
+
+
+	--problem
         let a = Data.List.Split.chunksOf 10 list
-        print a
         let b = V.toList $ V.fromList [ V.fromList [1|x<-[1..10]]|x<-[1..10] ]
         let c = [ V.fromList [1|x<-[1..10]]|x<-[1..10] ]
+        print list
+	print $ typeOf list
         print a
+        print $ typeOf a
         print b
+        print $ typeOf b
         print c
-	print $ typeOf a
-        --print $ typeOf b
-        --print $ typeOf c
+        print $ typeOf c
         hClose handle   
+
 
 f :: [String] -> [Int]
 f = Prelude.map read
